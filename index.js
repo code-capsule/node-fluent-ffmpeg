6c180868dbc07529ed1b3f664b953cf81f7ef813
彭道宽 <1063137960@qq.com>
2023-06-11 15:15:33 +0800

    修复  Unable to resolve some modules: "./lib-cov/fluent-ffmpeg" 的问题
    https://github.com/fluent-ffmpeg/node-fluent-ffmpeg/issues/573#issuecomment-1578026754

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require(`./lib${process.env.FLUENTFFMPEG_COV ? '-cov' : ''}/fluent-ffmpeg`);
+// module.exports = require(`./lib${process.env.FLUENTFFMPEG_COV ? '-cov' : ''}/fluent-ffmpeg`);
+module.exports = require('./lib/fluent-ffmpeg');
